1d9f17576b7e15fef62700e16147bda4ccf6907e
Neel <neelm88@gmail.com>
2019-10-05 23:53:49 -0400

    Moved get_user_preferences

--- get_compiled_info.rb
+++ get_compiled_info.rb
@@ -1,10 +1,7 @@
 # File created 10/05/2019 by Sharon Qiu
-# # Edited 10/04/2019 by Sri Ramya Dandu
+# Edited 10/04/2019 by Sri Ramya Dandu
+# Edited 10/05/2019 by Neel Mansukhani: moved get_user_preferences
 # Puts all methods that compile returned information together.
-
-require_relative 'info_scrape'
-require_relative 'user'
-require_relative 'utilities'
 
 # Created 09/26/2019 by Leah Gillespie
 # Edited 09/26/2019 by Neel Mansukhani: Returns sport schedule instead of printing
@@ -42,44 +39,4 @@ def get_search_keywords
     end
     keywords
 end
-# TODO: Make class method of user?
-# Created 09/26/2019 by Neel Mansukhani
-# Edited 10/04/2019 by Sri Ramya Dandu: Factored input to functions
-# Edited 10/05/2019 by Sri Ramya Dandu: Fixed case issues
-# Gets user info and creates file containing email contents
-def get_user_preferences(sports_reg_ex)
-    print "Please enter a username: "
-    username = gets.chomp # TODO: Check if username already exists and don't ask for email on return of user
-    print "Please enter a valid email address: "
-    email = gets.chomp
-    while !isValidEmail? email # TODO: Create Regex in utilities
-        puts "Please enter a valid email address: "
-        email = gets.chomp
-    end
-    # TODO: Add how often they want emails
-    yes_no = "Y"
-    sports = []
-    while yes_no == "Y"
 
-        # TODO: Add display list of sports.
-        # TODO: Add error msg.
-        sports.push(get_sport_choice sports_reg_ex)
-        yes_no = yes_no_input "Would you like to add another sport? (Y/N): "
-    end
-    s_n_b = ""
-    while s_n_b != "schedule" && s_n_b != "news" && s_n_b != "both"
-        print "Please enter 'Schedule' for schedule information, 'News' for news, or 'Both' for both: "
-        s_n_b = gets.chomp.downcase
-    end
-    info = []
-    case s_n_b
-    when "schedule"
-        info.push"Schedule"
-    when "news"
-        info.push"News"
-    when "both"
-        info.push"Schedule"
-        info.push"News"
-    end
-    User.new username, email, sports, info
-end
--- web_scraping.rb
+++ web_scraping.rb
@@ -9,14 +9,17 @@
 # Edited 10/04/2019 by Sri Ramya Dandu
 # Edited 10/05/2019 by Sri Ramya Dandu
 # Edited 10/05/2019 by Sharon Qiu
+# Edited 10/05/2019 by Neel Mansukhani
 
 # TODO: Documentation for all functions
 require 'time'
-require_relative 'info_scrape'
-require_relative 'user'
+require 'net/smtp'
 require_relative 'utilities'
+require_relative 'info_scrape'
 require_relative 'get_compiled_info'
-# TODO:  Rename this file
+require_relative 'user'
+
+
 # TODO: Move functions to other file
 # TODO: all gets to upper case
 
@@ -28,49 +31,48 @@ require_relative 'get_compiled_info'
 # Edited 10/04/2019 by Sri Ramya Dandu: Modified input and factored into functions
 # Edited 10/05/2019 by Sri Ramya Dandu: Fixed case issues
 # Edited 10/05/2019 by Sharon Qiu: Implemented news query search.
-if __FILE__ == $0
-  puts "Gathering information..."
-  start = Time.now
+# Edited 10/05/2019 by Neel Mansukhani: Removed if __FILE__
+puts "Gathering information..."
+start = Time.now
 
-  all_schedules_and_news = all_sports_schedules_and_news
-  schedules = all_schedules_and_news[:schedules]
-  news_info = all_schedules_and_news[:news]
+all_schedules_and_news = all_sports_schedules_and_news
+schedules = all_schedules_and_news[:schedules]
+news_info = all_schedules_and_news[:news]
 
-  ending = Time.now
-  puts "Finished gathering information. Time taken: #{ending-start} seconds."
-  
-  continue = "Y"
-  sports_reg_ex = sport_reg_exp schedules
-  list_sports sports_reg_ex
+ending = Time.now
+puts "Finished gathering information. Time taken: #{ending-start} seconds."
 
-  while continue == "Y"
+continue = "Y"
+sports_reg_ex = sport_reg_exp schedules
+list_sports sports_reg_ex
 
-    if (yes_no_input "Would you like to receive emails? (Y/N):") == "Y"
-      user = get_user_preferences sports_reg_ex
-      user.create_email news_info, schedules
-      puts 'Email Successfully Created'
-    end
-    sport = get_sport_choice sports_reg_ex
-    s_n_b = ""
-    while s_n_b != "schedule" && s_n_b != "news" && s_n_b != "both"
-      print "Please enter 'Schedule' for schedule information, 'News' for news, or 'Both' for both: "
-      s_n_b = gets.chomp.downcase
-    end
-    case s_n_b
-    when "schedule"
-      schedule = get_schedule sport, schedules
-      schedule.display nil
-    when "news"
-      kw = get_search_keywords
-      news = get_news sport, news_info
-      news.display kw
-    when "both"
-      kw = get_search_keywords
-      schedule = get_schedule sport, schedules
-      schedule.display nil
-      news = get_news sport, news_info
-      news.display kw
-    end
-    continue = yes_no_input "Would you like more information? (Y/N):"
+while continue == "Y"
+
+  if (yes_no_input "Would you like to receive emails? (Y/N):") == "Y"
+    user = User.get_user_preferences sports_reg_ex
+    user.create_email news_info, schedules
+    puts 'Email Successfully Created'
   end
+  sport = get_sport_choice sports_reg_ex
+  s_n_b = ""
+  while s_n_b != "schedule" && s_n_b != "news" && s_n_b != "both"
+    print "Please enter 'Schedule' for schedule information, 'News' for news, or 'Both' for both: "
+    s_n_b = gets.chomp.downcase
+  end
+  case s_n_b
+  when "schedule"
+    schedule = get_schedule sport, schedules
+    schedule.display nil
+  when "news"
+    kw = get_search_keywords
+    news = get_news sport, news_info
+    news.display kw
+  when "both"
+    kw = get_search_keywords
+    schedule = get_schedule sport, schedules
+    schedule.display nil
+    news = get_news sport, news_info
+    news.display kw
+  end
+  continue = yes_no_input "Would you like more information? (Y/N):"
 end
